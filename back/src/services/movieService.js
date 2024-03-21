004fd6b35e117f3f20b6493c05ca6acc23afde78
Toojeronix <manajeronimo7@gmail.com>
2024-03-21 14:37:01 -0300

    entrga

--- back/src/services/movieService.js
+++ back/src/services/movieService.js
@@ -3,7 +3,8 @@ let pel = []
 
 module.exports = {
     getMovie: async (genreId) => {
-        const movies = await Movie.find({ genre_ids: { $in: [genreId] } });
+
+        const movies = await Movie.find({ genre_ids: Number(genreId) });
         return movies
     },
     addMovie: async ({ backdrop_path, genre_ids, original_language, original_title, overview, popularity, poster_path, release_date, title, vote_average, vote_count }) => {
